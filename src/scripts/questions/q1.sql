55fac800f37cedd8a26e8342197edc34053c4cbc
guillaumearnx <37373941+guillaumearnx@users.noreply.github.com>
2021-10-23 00:01:18 +0200

    Move packages and files

--- src/scripts/questions/q1.sql
+++
@@ -1,6 +0,0 @@
-select distinct modele, V.NO_IMM
-from VEHICULE V, CALENDRIER C
-where CODE_CATEG = 'c3'
-and V.NO_IMM = C.NO_IMM
-and DATEJOUR between to_date('2015-10-23', 'YYYY/MM/DD') and to_date('2015-10-25', 'YYYY/MM/DD')
-and PASLIBRE is null;
