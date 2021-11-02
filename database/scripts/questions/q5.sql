e896ed95d93514f37d390484ca2767e10ff94f0a
shvde <72690981+shvde@users.noreply.github.com>
2021-11-02 11:13:08 +0100

    correc Q1 et Q5

--- database/scripts/questions/q5.sql
+++ database/scripts/questions/q5.sql
@@ -1,7 +1,9 @@
-select distinct C.NOM, C.VILLE, C.CODPOSTAL from CLIENT C
+select c.CODE_CLI, count( distinct v.MODELE) from CLIENT C
 inner join DOSSIER D on C.CODE_CLI = D.CODE_CLI
-inner join VEHICULE V on V.NO_IMM = D.NO_IMM
-where (select count(MODELE) from VEHICULE) = 2;
+inner join VEHICULE v on v.NO_IMM = d.NO_IMM
+having count(distinct v.MODELE) = 2
+group by c.CODE_CLI
+order by c.CODE_CLI;
 
 
 
